cbcf74e2143dff404c2153a03dc3f1af209aceac
Rodrigo Lessa <rodlessa@users.noreply.github.com>
2018-05-15 12:16:08 -0300

    Magic8
    Our first code in Python

---
+++ magic8.py
@@ -0,0 +1,28 @@
+import sys
+import random
+
+ans = True
+
+while ans:
+    question = input("Ask anything for me (Press enter to quit).")
+
+    answer = random.randint(1,8)
+
+    if question == "":
+        sys.exit()
+    elif answer == 1:
+        print ("Looks good")
+    elif answer == 2:
+        print ("Looks good")
+    elif answer == 3:
+        print ("Looks good")
+    elif answer == 4:
+        print ("Looks good")
+    elif answer == 5:
+        print ("Looks good")
+    elif answer == 6:
+        print ("Looks good")
+    elif answer == 7:
+        print ("Looks good")
+    elif answer == 8:
+        print ("Looks good")
